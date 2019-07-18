e5cbadff4496d006448bffd0d03162b3e6b5a5ff
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-18 13:13:27 -0400

    added header image that size is responsive to any device screen size

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.headerText}>Welcome!</Text>
+      <Image style={styles.imageHeader} source={require("./assets/main.jpg")} />
     </View>
   );
 }
@@ -12,8 +14,18 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fcec01',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText:{
+    color: '#114260',
+    fontWeight: 'bold',
+    fontSize: 32,
+  },
+  imageHeader:{
+    height: hp('30%'),
+    width: wp('100%'),
+
+  }
 });
