8eed7a20cbd48a5bbfe94f77a251dc02a4a725f4
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-30 19:20:53 -0400

    final format

--- app/config/AppNavigator.js
+++ app/config/AppNavigator.js
@@ -10,3 +10,5 @@
 // });
 //
 // export default AppNavigator
+
+//saving for later
--- app/config/navigation.js
+++ app/config/navigation.js
@@ -1,33 +1,36 @@
 import React from "react";
 import { TabNavigator } from "react-navigation";
 
-import Home from '../views/Home';
-import Services from '../views/Services';
-import Contact from '../views/Contact';
+import Home from "../views/Home";
+import Services from "../views/Services";
+import Contact from "../views/Contact";
 
-export const BottomTab = TabNavigator({
-  Home: {
-    screen: Home,
+export const BottomTab = TabNavigator(
+  {
+    Home: {
+      screen: Home
+    },
+    Services: {
+      screen: Services
+    },
+    Contact: {
+      screen: Contact
+    }
   },
-  Services: {
-    screen: Services,
-  },
-  Contact: {
-    screen: Contact,
-  }
-}, {
-  tabBarPosition: 'bottom',
-  swipeEnabled: true,
-  tabBarOptions: {
-    activeTintColor: '#114260',
-    activeBackgroundColor: "#fcec01",
-    inactiveTintColor: '#114260',
-    inactiveBackgroundColor: "#ffffcc",
+  {
+    tabBarPosition: "bottom",
+    swipeEnabled: true,
+    tabBarOptions: {
+      activeTintColor: "#114260",
+      activeBackgroundColor: "#fcec01",
+      inactiveTintColor: "#114260",
+      inactiveBackgroundColor: "#ffffcc",
 
-    labelStyle: {
-      fontSize: 23,
-      fontWeight: 'bold',
-      paddingBottom: 5,
+      labelStyle: {
+        fontSize: 23,
+        fontWeight: "bold",
+        paddingBottom: 5
+      }
     }
   }
-});
+);
