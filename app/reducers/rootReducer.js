2e6dcb3b45f70670c23b2553ff206877d4aecc41
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-30 19:20:21 -0400

    more format now that the app is on VScode

--- app/reducers/rootReducer.js
+++ app/reducers/rootReducer.js
@@ -1,11 +1,9 @@
-import { combineReducers } from 'redux'
-
+import { combineReducers } from "redux";
 
 const baseReducer = (state = 0, action) => {
   return state + 1;
-}
+};
 
 export default combineReducers({
-  baseReducer,
-
-})
+  baseReducer
+});
