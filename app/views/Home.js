9fe5547bf144d3f348a72d99565338cf52fd3790
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-31 13:39:53 -0400

    added button that when clicked opens the location of the office on maps app so that the user can quickly set up directions

--- app/views/Home.js
+++ app/views/Home.js
@@ -41,11 +41,27 @@ class Home extends Component {
             <View style={styles.emailContainer}>
               <TouchableOpacity
                 style={styles.callButton}
-                onPress={() => Linking.openURL("mailto:office@adcofnorton.com")}
+                onPress={() =>
+                  Linking.openURL(
+                    "mailto:office@adcofnorton.com?subject=App-email"
+                  )
+                }
               >
                 <Text style={styles.callButtonText}>
                   Click here to email us!
                 </Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.directionsContainer}>
+              <TouchableOpacity
+                style={styles.callButton}
+                onPress={() =>
+                  Linking.openURL(
+                    "https://www.google.com/maps/place/Advanced+Dental+Care+of+Norton/@41.9615083,-71.2001785,17z/data=!3m1!4b1!4m5!3m4!1s0x89e461d156c36c9f:0xb5fcf264919e17a5!8m2!3d41.9615083!4d-71.1979898"
+                  )
+                }
+              >
+                <Text style={styles.callButtonText}>Directions!</Text>
               </TouchableOpacity>
             </View>
           </View>
@@ -105,6 +121,18 @@ const styles = StyleSheet.create({
     paddingBottom: 30
   },
   emailContainer: {
+    paddingTop: 40,
+    marginTop: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    backgroundColor: "#fcec01",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  directionsContainer: {
     paddingTop: 40,
     marginTop: 10,
     paddingTop: 15,
