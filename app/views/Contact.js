609fbd6b8004b024738f4654e06a63771c89fce2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-07-31 13:25:32 -0400

    added button styling for forms

--- app/views/Contact.js
+++ app/views/Contact.js
@@ -10,6 +10,10 @@ import {
   Linking
 } from "react-native";
 import { SafeAreaView } from "react-navigation";
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp
+} from "react-native-responsive-screen";
 
 class Contact extends Component {
   constructor(props) {
@@ -23,34 +27,41 @@ class Contact extends Component {
           <View style={styles.headerDiv}>
             <Text style={styles.header}>Contact</Text>
           </View>
-          <Text>Forms</Text>
-          <TouchableOpacity
-            onPress={() =>
-              Linking.openURL(
-                "https://www.ident.ws/template_include/new_patient_sign_in.do?site=14740&practiceId=22404"
-              )
-            }
-          >
-            <Text>New Patient Form</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              Linking.openURL(
-                "https://www.adcofnorton.com/Content/Patient-Forms/Financial-Policy-2012.pdf"
-              )
-            }
-          >
-            <Text>Financial Policy Form</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() =>
-              Linking.openURL(
-                "https://www.adcofnorton.com/Content/Patient-Forms/ADC-Medical-Spa-Patient-Information.pdf"
-              )
-            }
-          >
-            <Text>Spa Patient Form</Text>
-          </TouchableOpacity>
+          <View style={styles.headerSpacing} />
+
+          <View style={styles.formsContainer}>
+            <Text style={styles.formHeader}>Forms</Text>
+            <TouchableOpacity
+              style={styles.formsText}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.ident.ws/template_include/new_patient_sign_in.do?site=14740&practiceId=22404"
+                )
+              }
+            >
+              <Text style={styles.formButtonText}>New Patient</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.formsText}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.adcofnorton.com/Content/Patient-Forms/Financial-Policy-2012.pdf"
+                )
+              }
+            >
+              <Text style={styles.formButtonText}>Financial Policy</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+              style={styles.formsText}
+              onPress={() =>
+                Linking.openURL(
+                  "https://www.adcofnorton.com/Content/Patient-Forms/ADC-Medical-Spa-Patient-Information.pdf"
+                )
+              }
+            >
+              <Text style={styles.formButtonText}>Spa Patient Info</Text>
+            </TouchableOpacity>
+          </View>
         </ScrollView>
       </SafeAreaView>
     );
@@ -71,6 +82,38 @@ const styles = StyleSheet.create({
     textAlign: "center",
     color: "#114260",
     marginTop: 15
+  },
+  headerSpacing: {
+    paddingTop: 30
+  },
+  formsContainer: {
+    alignItems: "center",
+    flex: 1
+  },
+  formHeader: {
+    fontSize: 25,
+    color: "#114260",
+    fontWeight: "bold"
+  },
+  formsText: {
+    paddingTop: 40,
+    marginTop: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+    marginLeft: 30,
+    marginRight: 30,
+    backgroundColor: "#fcec01",
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: "#fff"
+  },
+  formButtonText: {
+    color: "#114260",
+    fontWeight: "bold",
+    fontSize: 20,
+    backgroundColor: "#fcec01",
+    width: wp("70%"),
+    textAlign: "center"
   }
 });
 
