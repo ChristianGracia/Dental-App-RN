03ef950aa24b73dc004ebd34e70b3eff83671d43
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-01 11:56:08 -0400

    added veneer pic and info

--- app/views/Services.js
+++ app/views/Services.js
@@ -293,6 +293,15 @@ class Services extends React.Component {
                 }}
               />
             ) : null}
+            {this.state.showVeneers ? (
+              <Image
+                style={{ width: wp("90%"), height: hp("70%") }}
+                source={{
+                  uri:
+                    "https://www.dentalhealth.org/GetImage.aspx?IDMF=f95a3006-fe99-4190-9d4c-67d97e9c347f&w=400&h=549&src=mc"
+                }}
+              />
+            ) : null}
           </View>
 
           {/* Service Text */}
@@ -412,7 +421,24 @@ class Services extends React.Component {
               </Text>
             ) : null}
             {this.state.showVeneers ? (
-              <Text style={styles.itemPickText}>Veneers text goes here</Text>
+              <Text style={styles.itemPickText}>
+                Veneers are a thin layer of porcelain/composite material that is
+                designed to fit over the front surface of a tooth. Veneers can
+                improve the color, shape, position of tooth, and close gaps that
+                would normally require braces.
+                {"\n"}
+                {"\n"}
+                First a small amount of enamel (just enough to fit the Veneer)
+                is removed from the tooth where the Veneer is being placed. This
+                is done so that once the Veneer is placed, the tooth will be its
+                normal size. Then an impression will be taken and a Veneer will
+                be created by our lab and matched to the color of your
+                surrounding teeth.
+                {"\n"}
+                {"\n"}
+                Veneers can last for a very long time and quickly help solve
+                problem areas in your smile.
+              </Text>
             ) : null}
           </View>
           {/* Back Button */}
