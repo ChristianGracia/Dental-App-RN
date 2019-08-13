e732d2491d5d1906b860d0235f01bb6b49556f65
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-13 11:02:42 -0400

    check

--- app/index.js
+++ app/index.js
@@ -1,31 +1,27 @@
-import React from 'react';
-
+import React from "react";
 import { BottomTab } from "./config/navigation";
 
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
-import rootReducer from './reducers/rootReducer';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./reducers/rootReducer";
 
 //remove warnings during dev
 console.disableYellowBox = true;
 
-
 const store = createStore(rootReducer);
-
 
 export default class App extends React.Component {
   constructor(props) {
-    super(props)
+    super(props);
 
-    this.state = {
-    }
+    this.state = {};
   }
 
-  render(){
-    return(
-
-  <Provider store={store}>
-    <BottomTab />
-  </Provider>
-)}
+  render() {
+    return (
+      <Provider store={store}>
+        <BottomTab />
+      </Provider>
+    );
+  }
 }
