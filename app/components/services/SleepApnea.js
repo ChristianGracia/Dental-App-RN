2f4557e2c3af081118d4de22b176d42cb9b29391
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-13 11:57:36 -0400

    added sleep apnea image and more info

--- app/components/services/SleepApnea.js
+++ app/components/services/SleepApnea.js
@@ -10,7 +10,17 @@ class SleepApnea extends Component {
     return (
       <View>
         <Text style={styles.itemPick}>Sleep Apnea</Text>
+        <Image
+          style={{ width: "100%", height: hp("45%"), marginTop: 15 }}
+          resizeMode="contain"
+          source={{
+            uri:
+              "https://cdn.foreversites.com/assets/img/infographics/Sleep-Apnea-Infographic-cropped.png"
+          }}
+        />
         <Text style={styles.itemPickText}>
+          {"\n"}
+          {"\n"}
           Sleep apnea is an extremely serious condition that causes a person to
           wake up constantly throughout the night from respiratory disturbances
           like sleep apnea (when you stop breathing).
@@ -21,9 +31,33 @@ class SleepApnea extends Component {
           doctor to manage.
           {"\n"}
           {"\n"}
-          Hypertension (high blood pressure, eventually renal failure),
-          diabetes, heart failure, and stroke are very common in people who
-          suffer from sleep apnea.
+          <Text
+            style={{
+              color: "red",
+              textAlign: "center",
+              fontSize: 25,
+              fontWeight: "bold"
+            }}
+          >
+            Types of Sleep Apnea {"\n"}
+            {"\n"}
+          </Text>
+          <Text style={{ fontWeight: "bold" }}>
+            {"\n"}Obstructive sleep apnea:{"\n"}
+          </Text>
+          More common form that occurs when throat muscles relax
+          {"\n"}
+          {"\n"}
+          <Text style={{ fontWeight: "bold" }}>Central sleep apnea:{"\n"}</Text>
+          When your brain doesn't send proper signals to the muscles that
+          control breathing
+          {"\n"}
+          {"\n"}
+          <Text style={{ fontWeight: "bold" }}>
+            Complex sleep apnea syndrome:{"\n"}
+          </Text>
+          Known as treatment-emergent central sleep apnea, which occurs when
+          someone has both obstructive sleep apnea and central sleep apnea
           {"\n"}
           {"\n"}
           <Text style={{ color: "red", fontWeight: "bold" }}>
@@ -39,12 +73,18 @@ class SleepApnea extends Component {
           </Text>
           {"\n"}
           {"\n"}
+          Hypertension (high blood pressure, eventually renal failure),
+          diabetes, heart failure, and stroke are very common in people who
+          suffer from sleep apnea.
+          {"\n"}
+          {"\n"}
           <Text
             style={{
               color: "blue",
               textAlign: "center",
               fontWeight: "bold",
-              fontSize: "15"
+              fontSize: "30",
+              paddingTop: 30
             }}
             onPress={() =>
               Linking.openURL(
@@ -64,7 +104,8 @@ const styles = StyleSheet.create({
   itemPickText: {
     color: "#000",
     fontSize: 20,
-    paddingLeft: 20
+    paddingLeft: 15,
+    paddingRight: 15
   },
   itemPick: {
     textAlign: "center",
