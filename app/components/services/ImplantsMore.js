1df305a0d8e472d849542588f1cb444cac557881
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-15 12:03:19 -0400

    cleaned up implants more

--- app/components/services/ImplantsMore.js
+++ app/components/services/ImplantsMore.js
@@ -14,17 +14,17 @@ export default class ImplantsMore extends Component {
 
   render() {
     return (
-      <View style={{ paddingTop: 30, flex: 1 }}>
-           
-           <View>
-      
+      <View style={{ paddingTop: 30, flex: 1, alignItems: "center" }}>
+        <View>
           <Text style={{}}>
             If your jawbone is not able to support dental implants, several
             techniques can be used to rebuild bone, restore your natural jawline
             and provide a sturdy foundation for implant-supported teeth. These
             include:{"\n\n"}
           </Text>
+
           <Text style={styles.headerTech}>Bone graft{"\n\n"}</Text>
+
           <Text>
             If your jawbone isn't thick enough or is too soft, you may need bone
             grafting before you can have dental implant surgery. That's because
@@ -32,6 +32,7 @@ export default class ImplantsMore extends Component {
             your bone, and if it can't support the implant, the surgery likely
             would fail.
           </Text>
+
           <Image
             style={{
               width: wp("80%"),
@@ -45,6 +46,7 @@ export default class ImplantsMore extends Component {
                 "https://www.mayoclinic.org/-/media/kcms/gbs/patient-consumer/images/2013/08/26/10/37/my00084_im04754_mcdc7_jawbone_graftthu_jpg.png"
             }}
           />
+
           <Text>
             {"\n\n"}A bone graft can create a more solid base for the implant.
             There are several bone graft materials that can be used to rebuild a
@@ -59,12 +61,14 @@ export default class ImplantsMore extends Component {
             the implant surgery. The condition of your jawbone determines how
             you proceed.{"\n\n"}
           </Text>
+
           <Text style={styles.headerTech}>Sinus lift{"\n\n"}</Text>
           <Text>
             Also called sinus augmentation or sinus elevation, this involves
             adding bone below the sinus in cases where natural bone has
             deteriorated due to missing upper back teeth.{"\n\n"}
           </Text>
+
           <Image
             style={{
               width: wp("80%"),
@@ -79,11 +83,13 @@ export default class ImplantsMore extends Component {
             }}
           />
           <Text style={styles.headerTech}>Ridge expansion{"\n\n"}</Text>
+
           <Text>
             If your jaw isn’t wide enough to support dental implants, bone graft
             material can be added to a small ridge, or space, created along the
             top of your jaw.{"\n\n"}
           </Text>
+
           <Image
             style={{
               width: wp("80%"),
@@ -97,95 +103,117 @@ export default class ImplantsMore extends Component {
                 "https://www.jaipurdentalimplants.com/wp-content/uploads/2017/04/Ridge-Split.jpg"
             }}
           />
-          <Text style={styles.header}>Placing the dental implant{"\n\n"}</Text><Text>
-          During surgery to place the dental implant, your oral surgeon makes a
-          cut to open your gum and expose the bone. Holes are drilled into the
-          bone where the dental implant metal post will be placed. Since the
-          post will serve as the tooth root, it's implanted deep into the bone.
-          At this point, you'll still have a gap where your tooth is missing.
-          {"\n\n"} A type of partial, temporary denture can be placed for
-          appearance, if needed. You can remove this denture for cleaning and
-          while you sleep. Waiting for bone growth Once the metal implant post
-          is placed in your jawbone, osseointegration begins.
-          {"\n\n"}During this process, the jawbone grows into and unites with
-          the surface of the dental implant. This process, which can take
-          several months, helps provide a solid base for your new artificial
-          tooth — just as roots do for your natural teeth.
-          {"\n\n"}
+          <Text style={styles.header}>Placing the dental implant{"\n\n"}</Text>
+          <Text>
+            During surgery to place the dental implant, your oral surgeon makes
+            a cut to open your gum and expose the bone. Holes are drilled into
+            the bone where the dental implant metal post will be placed. Since
+            the post will serve as the tooth root, it's implanted deep into the
+            bone. At this point, you'll still have a gap where your tooth is
+            missing.
+            {"\n\n"} A type of partial, temporary denture can be placed for
+            appearance, if needed. You can remove this denture for cleaning and
+            while you sleep. Waiting for bone growth Once the metal implant post
+            is placed in your jawbone, osseointegration begins.
+            {"\n\n"}During this process, the jawbone grows into and unites with
+            the surface of the dental implant. This process, which can take
+            several months, helps provide a solid base for your new artificial
+            tooth — just as roots do for your natural teeth.
+            {"\n\n"}
           </Text>
+
           <Text style={styles.header}>Placing the abutment {"\n\n"}</Text>
           <Text>
-          When osseointegration is complete, you may need additional surgery to
-          place the abutment — the piece where the crown will eventually attach.
-          This minor surgery is typically done with local anesthesia in an
-          outpatient setting. To place the abutment:
-          {"\n\n"}</Text>
-          <Text style={styles.implantSteps}>1{") "}</Text><Text>Your oral surgeon
-          reopens your gum to expose the dental implant {"\n\n"}</Text>
-          <Text style={styles.implantSteps}>2{") "}</Text><Text>The abutment is
-        attached to the dental implant
-          {"\n\n"}</Text>
-          <Text style={styles.implantSteps}>3{") "}</Text><Text>The gum tissue is then
-          closed around, but not over, the abutment{"\n\n"}
-          {"\n\n"}
-          In some cases, the abutment is attached to the dental implant metal
-          post when the post is implanted. That means you won't need an extra
-          surgical step. Because the abutment juts past the gumline, however,
-          it's visible when you open your mouth — and it will be that way until
-          your dentist completes the tooth prosthesis.
-          {"\n\n"}
-          Some people don't like that appearance and prefer to have the abutment
-          placed in a separate procedure. After the abutment is placed, your
-          gums must heal for about two weeks before the artificial tooth can be
-          attached.{"\n\n"}
+            When osseointegration is complete, you may need additional surgery
+            to place the abutment — the piece where the crown will eventually
+            attach. This minor surgery is typically done with local anesthesia
+            in an outpatient setting. To place the abutment:
+            {"\n\n"}
           </Text>
+
+          <Text style={styles.implantSteps}>1{") "}</Text>
+          <Text>
+            Your oral surgeon reopens your gum to expose the dental implant{" "}
+            {"\n\n"}
+          </Text>
+          <Text style={styles.implantSteps}>2{") "}</Text>
+          <Text>
+            The abutment is attached to the dental implant
+            {"\n\n"}
+          </Text>
+
+          <Text style={styles.implantSteps}>3{") "}</Text>
+          <Text>
+            The gum tissue is then closed around, but not over, the abutment
+            {"\n\n"}
+            {"\n\n"}
+            In some cases, the abutment is attached to the dental implant metal
+            post when the post is implanted. That means you won't need an extra
+            surgical step. Because the abutment juts past the gumline, however,
+            it's visible when you open your mouth — and it will be that way
+            until your dentist completes the tooth prosthesis.
+            {"\n\n"}
+            Some people don't like that appearance and prefer to have the
+            abutment placed in a separate procedure. After the abutment is
+            placed, your gums must heal for about two weeks before the
+            artificial tooth can be attached.{"\n\n"}
+          </Text>
+
           <Text style={styles.header}>
             Choosing your new artificial teeth {"\n\n"}
           </Text>
+
           <Text>
-          Once your gums heal, you'll have more impressions made of your mouth
-          and remaining teeth. These impressions are used to make the crown —
-          your realistic-looking artificial tooth. The crown can't be placed
-          until your jawbone is strong enough to support use of the new tooth.
-          You and your dental specialist can choose artificial teeth that are
-          removable, fixed or a combination of both: {"\n\n"}{" "}
-        </Text>
-        </View>
-        <View style={styles.removeFixContainer}>
-          <Text style={{ fontSize: 20 }}>
-            <Text style={styles.removeOrFix}>Removable {"\n\n"}</Text>
-            <Text style={{ width: wp("80%") }}>
-              This type is similar to a conventional removable denture and can
-              be a partial or full denture. It contains artificial white teeth
-              surrounded by pink plastic gum. It's mounted on a metal frame
-              that's attached to the implant abutment, and it snaps securely
-              into place. It can be easily removed for repair or daily cleaning.{" "}
-              {"\n\n"}
-            </Text>
+            Once your gums heal, you'll have more impressions made of your mouth
+            and remaining teeth. These impressions are used to make the crown —
+            your realistic-looking artificial tooth. The crown can't be placed
+            until your jawbone is strong enough to support use of the new tooth.
+            You and your dental specialist can choose artificial teeth that are
+            removable, fixed or a combination of both: {"\n\n"}{" "}
           </Text>
-          <Text style={{ fontSize: 20 }}>
-            <Text style={styles.removeOrFix}>Fixed {"\n\n"}</Text>
-            <Text style={{ fontSize: 20, textAlign: "center" }}>
-              In this type, an artificial tooth is permanently screwed or
-              cemented onto an individual implant abutment. You can't remove the
-              tooth for cleaning or during sleep. Most of the time, each crown
-              is attached to its own dental implant. However, because implants
-              are exceptionally strong, several teeth can be replaced by one
-              implant if they're bridged together. {"\n\n"}
+
+          <View style={styles.removeFixContainer}>
+            <Text style={{ fontSize: 20 }}>
+              <Text style={styles.removeOrFix}>Removable {"\n\n"}</Text>
+              <Text style={{ width: wp("80%") }}>
+                This type is similar to a conventional removable denture and can
+                be a partial or full denture. It contains artificial white teeth
+                surrounded by pink plastic gum. It's mounted on a metal frame
+                that's attached to the implant abutment, and it snaps securely
+                into place. It can be easily removed for repair or daily
+                cleaning. {"\n\n"}
+              </Text>
             </Text>
-          </Text>
+            <Text style={{ fontSize: 20 }}>
+              <Text style={styles.removeOrFix}>Fixed {"\n\n"}</Text>
+              <Text style={{ fontSize: 20, textAlign: "center" }}>
+                In this type, an artificial tooth is permanently screwed or
+                cemented onto an individual implant abutment. You can't remove
+                the tooth for cleaning or during sleep. Most of the time, each
+                crown is attached to its own dental implant. However, because
+                implants are exceptionally strong, several teeth can be replaced
+                by one implant if they're bridged together. {"\n\n"}
+              </Text>
+            </Text>
+          </View>
+
           <Text style={styles.header}>
             Alternative Dental Implant Techniques
           </Text>
+
           <Text style={{ fontSize: 20 }}>
             {"\n\n"}
             Depending on the health of your jawbone and your specific needs,
             your dental implant dentist may suggest some alternative treatment
             options in addition to the traditional multi-step dental implant
             procedure. Options may include:{"\n\n"}
-            <Text style={{ fontWeight: "bold" }}>
-              Immediate Load Dental Implants
-            </Text>
+          </Text>
+
+          <Text style={{ fontWeight: "bold" }}>
+            Immediate Load Dental Implants
+          </Text>
+
+          <Text>
             {"\n\n"}
             Also called as same day implants or Teeth in a Day®, immediate load
             dental implants allow placement of a temporary tooth during the same
@@ -193,37 +221,41 @@ export default class ImplantsMore extends Component {
             option if you have enough natural bone and an implant secure enough
             to support immediate placement and pressure on the new temporary
             tooth.{"\n\n"}
-            <Text style={{ fontWeight: "bold" }}>
-              Mini dental implants (MDIs)
-            </Text>
-            <Text>
-              {"\n\n"}
-              Also called small or narrow diameter implants, these
-              toothpick-sized implants are narrower than most commonly used
-              dental implants. They are placed through less-invasive techniques
-              and are used primarily to stabilize a lower denture.{"\n\n"}
-            </Text>
-            <Text style={{ fontWeight: "bold" }}>All-on-4</Text>
-            <Text>
-              {"\n\n"}All-on-4 is an alternative to placing a top or bottom set
-              of replacement teeth, called a full arch. Four dental implants (6
-              for "All-on-6" available) are placed in available bone, avoiding
-              the need for bone grafting. Special abutments are used so that a
-              temporary set of replacement teeth can be placed the same day. You
-              follow a modified diet while the gum tissues heal and the implants
-              bond with your natural bone. After about six months, the permanent
-              replacement teeth will be placed and you can resume a regular
-              diet.
-              {"\n\n"}
-              When replacing multiple teeth, less implants are typically used to
-              support many teeth. For example, to replace the entire top row of
-              a patient’s teeth, 4 or 6 implants may be enough to support them
-              all. Similarly, three missing teeth can be supported by two
-              implants. The dental implants cost is typically higher when it
-              comes to replacing multiple teeth, but is definitely the most
-              beneficial and valuable option.
-            </Text>
-  
+          </Text>
+
+          <Text style={{ fontWeight: "bold" }}>
+            Mini dental implants (MDIs)
+          </Text>
+
+          <Text>
+            {"\n\n"}
+            Also called small or narrow diameter implants, these toothpick-sized
+            implants are narrower than most commonly used dental implants. They
+            are placed through less-invasive techniques and are used primarily
+            to stabilize a lower denture.{"\n\n"}
+          </Text>
+
+          <Text style={{ fontWeight: "bold" }}>All-on-4</Text>
+
+          <Text>
+            {"\n\n"}All-on-4 is an alternative to placing a top or bottom set of
+            replacement teeth, called a full arch. Four dental implants (6 for
+            "All-on-6" available) are placed in available bone, avoiding the
+            need for bone grafting. Special abutments are used so that a
+            temporary set of replacement teeth can be placed the same day. You
+            follow a modified diet while the gum tissues heal and the implants
+            bond with your natural bone. After about six months, the permanent
+            replacement teeth will be placed and you can resume a regular diet.
+            {"\n\n"}
+            When replacing multiple teeth, less implants are typically used to
+            support many teeth. For example, to replace the entire top row of a
+            patient’s teeth, 4 or 6 implants may be enough to support them all.
+            Similarly, three missing teeth can be supported by two implants. The
+            dental implants cost is typically higher when it comes to replacing
+            multiple teeth, but is definitely the most beneficial and valuable
+            option.
+          </Text>
+
           <View style={{ alignItems: "center" }}>
             <Image
               style={{
@@ -236,7 +268,7 @@ export default class ImplantsMore extends Component {
               }}
             />
           </View>
-   
+        </View>
       </View>
     );
   }
