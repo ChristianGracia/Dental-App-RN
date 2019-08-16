a1e957e596e53c5bb9c762714d74261f0a983357
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-16 15:45:45 -0400

    added a lot more info to aesthetic spa component

--- app/components/services/AestheticSpa.js
+++ app/components/services/AestheticSpa.js
@@ -25,16 +25,30 @@ class AestheticSpa extends Component {
             team of skilled nurses, we offer Botox, Dysport, and Dermal Fillers.
             {"\n"}
             {"\n"}
-            Botox and Dysport are both safe and effective treatments for
-            moderate to severe wrinkles.
-            {"\n"}
-            {"\n"}
-            These treatments reduce the appearance of wrinkles by relaxing the
-            underlying muscles beneath the skin which results in smoother skin.
-            {"\n"}
-            {"\n"}
-            Both treatments have the same active ingredient but have their
-            differences.{"\n\n"}
+            <Text
+              style={{
+                color: "blue",
+                fontSize: 25,
+                fontWeight: "bold",
+                textAlign: "center"
+              }}
+            >
+              Botox/Dysport
+            </Text>
+            <Text>
+              {"\n"}
+              {"\n"}Botox and Dysport are both safe and effective treatments for
+              moderate to severe wrinkles.
+              {"\n"}
+              {"\n"}
+              These treatments reduce the appearance of wrinkles by relaxing the
+              underlying muscles beneath the skin which results in smoother
+              skin.
+              {"\n"}
+              {"\n"}
+              Both treatments have the same active ingredient but have their
+              differences.{"\n\n"}
+            </Text>
             <Text
               style={{
                 color: "blue",
@@ -63,6 +77,52 @@ class AestheticSpa extends Component {
               Migraines/Headaches{"\n"}
               TMJ Teeth Grinding{"\n"}
               Excessive Underarm Sweating{"\n"}
+            </Text>
+            <Text
+              style={{
+                color: "blue",
+                fontSize: 25,
+                fontWeight: "bold",
+                textAlign: "center"
+              }}
+            >
+              {"\n\n"}
+              Dermal Fillers:{"\n\n"}
+            </Text>
+            <Text>
+              Dermal fillers help to replace lost volume, add contour, and help
+              to diminish fine lines in the face. This gives the face a more
+              youthful, rested appearance.
+              {"\n\n"}Injectable fillers are made of hyaluronic acid, which is a
+              material that is naturally within our own skin, tissues, and
+              joints, and as we age, we begin to lose it. Hyaluronic acid
+              moisturizes our skin, plays a role in elasticity, and give our
+              faces volume and structure.
+              {"\n\n"}Injectable fillers have a possibility of bruising,
+              swelling, and/or tenderness at the site of injection for
+              approximately five days. Depending on the type/brand, filler may
+              last up to one year.
+            </Text>
+            <Text
+              style={{
+                color: "blue",
+                fontSize: 25,
+                fontWeight: "bold",
+                textAlign: "center"
+              }}
+            >
+              {"\n\n"}
+              Dermal Filler Uses:
+              {"\n\n"}
+            </Text>
+            <Text style={{ color: "red", textAlign: "center" }}>
+              Lips Cheeks{"\n"}
+              Nasolabial Folds{"\n"}
+              Marionet Lines{"\n"}
+              Smoker Lines{"\n"}
+              Acne Scars{"\n"}
+              Under Eye {"(Hollow eyes)\n"}
+              Jawline Contouring Chin Temples{"\n"}
             </Text>
             <Text style={{ fontWeight: "bold", textAlign: "center" }}>
               {"\n\n"}More information about Botox, Dysport, and Dermal Fillers
