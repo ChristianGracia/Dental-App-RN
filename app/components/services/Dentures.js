9eea15cb7c68892efd43c9a414fd6bf1fea3a2f2
Christian Gracia <christiangracia.student@careerdevs.com>
2019-08-23 14:57:47 -0400

    denture component formatted

--- app/components/services/Dentures.js
+++ app/components/services/Dentures.js
@@ -61,13 +61,19 @@ class Dentures extends Component {
                 Overdenture
               </Text>
               {"\n\n"}
-              Sometimes some of your teeth can be saved to preserve your jawbone
-              and provide stability and support for the denture. An overdenture
-              fits over a small number of remaining natural teeth after they
-              have been prepared by your dentist. Implants can serve the same
-              function, too.
+              <Text>
+                Sometimes some of your teeth can be saved to preserve your
+                jawbone and provide stability and support for the denture. An
+                overdenture fits over a small number of remaining natural teeth
+                after they have been prepared by your dentist. Implants can
+                serve the same function, too.{"\n\n"}
+              </Text>
+            </Text>
+            <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+              Denture Care
             </Text>
             <Text>
+              {"\n\n"}
               New dentures may feel awkward for a few weeks until you become
               accustomed to them. The dentures may feel loose while the muscles
               of your cheek and tongue learn to keep them in place.{"\n\n"} It
@@ -77,9 +83,14 @@ class Dentures extends Component {
               Follow-up appointments with the dentist are generally needed after
               a denture is inserted so the fit can be checked and adjusted. If
               any problem persists, particularly irritation or soreness, be sure
-              to consult your dentist.
+              to consult your dentist. {"\n\n"}
             </Text>
+            <Text style={{ fontWeight: "bold", color: iOSColors.blue }}>
+              Denture Cleaning
+            </Text>
+
             <Text>
+              {"\n\n"}
               Like your teeth, your dentures should be brushed daily to remove
               food particles and plaque.{"\n\n"}Brushing also can help keep the
               teeth from staining.{"\n\n"}Rinse your dentures before brushing to
@@ -92,38 +103,37 @@ class Dentures extends Component {
               {"\n\n"}When you’re not wearing your dentures, put them in a safe
               place covered in water to keep them from warping.
             </Text>
-            <Text>
-              Occasionally, denture wearers may use adhesives. Adhesives come in
-              many forms: creams, powders, pads/wafers, strips or liquids. If
-              you use one of these products, read the instructions, and use them
-              exactly as directed.{"\n\n"}Your dentist can recommend appropriate
-              cleansers and adhesives; look for products with the ADA Seal of
-              Acceptance. Products with the ADA Seal have been evaluated for
-              safety and effectiveness.{"\n\n"}If you have any questions about
-              your dentures, or if they stop fitting well or become damaged,
-              contact your us! Be sure to schedule regular dental checkups, too!
-              {"\n\n"}
+            <Text style={{ fontStyle: "italic", color: iOSColors.red }}>
+              {"\n\n\n"}If you have any questions about your dentures, or if
+              they stop fitting well or become damaged, contact your us! Be sure
+              to schedule regular dental checkups, too!
+              {"\n\n\n"}
             </Text>
             <Text>
-              <Text style={{ fontWeight: "bold" }}>How Are Dentures Made?</Text>
+              <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+                How Are Dentures Made?
+              </Text>
               {"\n\n"}The denture development process takes a few weeks and
               several appointments. Once your dentist or prosthodontist (a
               dentist who specializes in the restoration and replacement of
-              teeth) determines what type of appliance is best for you.
+              teeth) determines what type of appliance is best for you.{"\n\n"}
             </Text>
-            <Text>Denture Creation</Text>
-            <Text>
-              Make a series of impressions of your jaw and take measurements of
-              how your jaws relate to one another and how much space is between
-              them.{"\n\n"}Create models, wax forms, and/or plastic patterns in
-              the exact shape and position of the denture to be made.
-              {"\n\n"}You will "try in" this model several times and the denture
-              will be assessed for color, shape, and fit before the final
-              denture is cast.{"\n\n"}Create a final denture.
-              {"\n\n"}Make adjustments to fit if/as necessary{"\n\n"}
+            <Text style={{ fontWeight: "bold", color: iOSColors.blue }}>
+              Denture Creation
             </Text>
             <Text>
-              <Text style={{ fontWeight: "bold" }}>
+              {"\n\n1) "}Make a series of impressions of your jaw and take
+              measurements of how your jaws relate to one another and how much
+              space is between them.{"\n\n2) "}Create models, wax forms, and/or
+              plastic patterns in the exact shape and position of the denture to
+              be made.
+              {"\n\n3) "}You will "try in" this model several times and the
+              denture will be assessed for color, shape, and fit before the
+              final denture is cast.{"\n\n4) "}Create a final denture.
+              {"\n\n5) "}Make adjustments to fit if/as necessary{"\n\n"}
+            </Text>
+            <Text>
+              <Text style={{ fontWeight: "bold", textAlign: "center" }}>
                 Will Eating With New Dentures Be Difficult?
               </Text>
               {"\n\n"}Eating with new dentures will take a little practice and
@@ -133,10 +143,12 @@ class Dentures extends Component {
               {"\n\n"}As you get used to new dentures, add other foods until you
               return to a normal diet. Be cautious with hot or hard foods and
               sharp-edged bones or shells. Avoid foods that are extremely sticky
-              or hard.
+              or hard.{"\n\n"}
             </Text>
             <Text>
-              <Text>Are Dentures Worn 24 Hours a Day?</Text>
+              <Text style={{ fontWeight: "bold", textAlign: "center" }}>
+                Are Dentures Worn 24 Hours a Day?
+              </Text>
               {"\n\n"}Your dentist or prosthodontist will instruct you as to how
               long to wear dentures and when to remove them. During the first
               several days after receiving your denture, you may be asked to
