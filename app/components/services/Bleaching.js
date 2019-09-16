30fe049f956356e7add6ebd64141788578c01f11
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-16 12:23:32 -0400

    added sheer white teeth whitening strips info

--- app/components/services/Bleaching.js
+++ app/components/services/Bleaching.js
@@ -34,25 +34,34 @@ class Bleaching extends Component {
             their teeth.
           </Text>
           <View style={{ textAlign: "center", alignItems: "center" }}>
-            <Text
-              style={{
-                color: iOSColors.blue,
-                fontSize: 30,
-                fontWeight: "bold"
-              }}
-            >
-              Glo Whitening
-            </Text>
-            <Text
-              style={{
-                color: iOSColors.blue,
-                fontSize: 30,
-                fontWeight: "bold"
-              }}
-            >
-              Sheer White Strips
+            <Text style={styles.bleachHeader}>Glo</Text>
+            <Text style={styles.bleachHeader}>Sheer White Strips</Text>
+            <Text style={styles.itemPickText}>
+              Sheer White Whitening Strips are teeth whitening strips that are
+              worn for just 1-2 hours and acheive noticable whitening results.
+              {"\n\n"}
+              These strips stick and mold perfectly to the teeth, becoming
+              almost invisible. Not only are they comfortable to wear during any
+              activity, including talking or drinking cold beverages, but they
+              also cause little or no sensitivity!
+              {"\n\n"}These white strips can usually make your teeth about 5
+              shades lighter in just 5 days!
             </Text>
           </View>
+          <Text style={styles.itemHeader}>How to use</Text>
+          <Text style={styles.itemPickText}>
+            • Open package, whitening formula is face up.
+            {"\n\n"}• Remove strip from tray.
+            {"\n\n"}• Center strip, place top edge of strip at gum line.
+            {"\n\n"}• Fold strip to back side of teeth. Press firmly on all
+            surfaces. Warmth and moisture from your mouth will mold the strip to
+            your teeth.
+            {"\n\n"}• Wear for 1-2 hours. May continue to speak, do light
+            activities, and drink cool beverages.
+            {"\n\n"}• To remove, grasp one corner and pull down away from the
+            front of teeth, then push strip away from the back of teeth.
+            {"\n\n"}• Brush or rinse away any remaining gel
+          </Text>
         </View>
       </View>
     );
@@ -85,6 +94,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#fcec01"
+  },
+  bleachHeader: {
+    color: iOSColors.blue,
+    fontSize: 30,
+    fontWeight: "bold"
   }
 });
 
