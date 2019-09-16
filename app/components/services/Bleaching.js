7d7781b803acedb64354343d94bf3355f5386468
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-16 12:06:39 -0400

    added info and style to bleaching page

--- app/components/services/Bleaching.js
+++ app/components/services/Bleaching.js
@@ -4,13 +4,14 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
+import { iOSColors } from "react-native-typography";
 
 class Bleaching extends Component {
   render() {
     return (
       <View>
         <Text style={styles.itemPick}>Bleaching</Text>
-        <View style={{ padding: 15 }}>
+        <View>
           <Image
             style={styles.imgSizer}
             resizeMode="contain"
@@ -19,7 +20,39 @@ class Bleaching extends Component {
                 "https://cdn.prod-carehubs.net/n1/802899ec472ea3d8/uploads/2015/05/teeth-whitening.jpg"
             }}
           />
-          <Text style={styles.itemPickText}>Bleaching text</Text>
+          <Text style={styles.itemPickText}>
+            At Advanced Dental Care of Norton we can help make your teeth
+            whiter!{"\n\n"}
+            As we get older and consume known stain causing items such as
+            coffee, wine, food, and tobacco, our teeth becoming less white is
+            inevitable.{"\n\n"}
+            But the good news is that you can do something about it!{"\n"}
+          </Text>
+          <Text style={styles.itemHeader}>Bleaching Solutions</Text>
+          <Text style={styles.itemPickText}>
+            {"\n"}Currently we offer two solutions to patients that want white
+            their teeth.
+          </Text>
+          <View style={{ textAlign: "center", alignItems: "center" }}>
+            <Text
+              style={{
+                color: iOSColors.blue,
+                fontSize: 30,
+                fontWeight: "bold"
+              }}
+            >
+              Glo Whitening
+            </Text>
+            <Text
+              style={{
+                color: iOSColors.blue,
+                fontSize: 30,
+                fontWeight: "bold"
+              }}
+            >
+              Sheer White Strips
+            </Text>
+          </View>
         </View>
       </View>
     );
@@ -28,8 +61,9 @@ class Bleaching extends Component {
 
 const styles = StyleSheet.create({
   itemPickText: {
-    color: "#000",
-    fontSize: 20
+    color: "#114260",
+    fontSize: 20,
+    padding: 15
   },
   itemPick: {
     textAlign: "center",
@@ -44,6 +78,13 @@ const styles = StyleSheet.create({
     width: "100%",
     height: hp("45%"),
     marginTop: 15
+  },
+  itemHeader: {
+    color: "#114260",
+    fontSize: 25,
+    fontWeight: "bold",
+    textAlign: "center",
+    backgroundColor: "#fcec01"
   }
 });
 
