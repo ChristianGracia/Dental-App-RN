66e8e983f801ff74a37ba6ec9b84043d153a5980
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-16 14:27:33 -0400

    turned app from a class based component to functional

--- app/index.js
+++ app/index.js
@@ -20,18 +20,10 @@ console.disableYellowBox = true;
 
 const store = createStore(rootReducer);
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <Provider store={store}>
-        <BottomTab />
-      </Provider>
-    );
-  }
+export default function App() {
+  return (
+    <Provider store={store}>
+      <BottomTab />
+    </Provider>
+  );
 }
