1de6bcd9d46a66559794a721b43083ad130e82e8
Christian Gracia <christiangracia.student@careerdevs.com>
2019-09-20 14:06:39 -0400

    partial denture page okay for v1.0

--- app/components/services/Partials.js
+++ app/components/services/Partials.js
@@ -4,15 +4,34 @@ import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp
 } from "react-native-responsive-screen";
+import { iOSColors } from "react-native-typography";
 
 class Partials extends Component {
   render() {
     return (
       <View>
         <Text style={styles.itemPick}>Partial Dentures</Text>
-        <View style={{ padding: 15 }}>
+        <View style={{}}>
+          <Image
+            style={styles.imgSizer}
+            resizeMode="contain"
+            source={{
+              uri:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj6SvJSz85siKS2H0mXkufvdFoZb77X54TvWMbDWUU_agG4MtV"
+            }}
+          />
           <Text style={styles.itemPickText}>
-            Partial Denture text goes here
+            A removable partial denture usually consists of replacement teeth
+            attached to a pink or gum-colored plastic base and can be connected
+            by metal framework that holds the denture in place in the mouth.
+            {"\n\n"}Partial dentures are used when one or more natural teeth
+            remain in the upper or lower jaw. Not only does a partial denture
+            fill in the spaces created by missing teeth, it prevents other teeth
+            from changing position.
+          </Text>
+          <Text style={{ ...styles.itemPickText, color: iOSColors.blue }}>
+            Partial dentures are a great way to cover missing teeth and restore
+            your smile!
           </Text>
         </View>
       </View>
@@ -23,7 +42,8 @@ class Partials extends Component {
 const styles = StyleSheet.create({
   itemPickText: {
     color: "#114260",
-    fontSize: 20
+    fontSize: 20,
+    padding: 15
   },
   itemPick: {
     textAlign: "center",
@@ -40,6 +60,11 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     backgroundColor: "#fcec01"
+  },
+  imgSizer: {
+    width: "100%",
+    height: hp("45%"),
+    marginTop: 15
   }
 });
 
